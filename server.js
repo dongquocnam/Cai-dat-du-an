0f535027f3f6df8b31c72ecc0c69054f42203680
dongquocnam <namb1906718@student.ctu.edu.vn>
2022-09-27 18:11:03 +0700

    Cai dat cac handler truy xuat CSDL

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const app = require("./app");
 const config = require("./app/config");
+const MongoDB = require("./app/untils/mongodb.utils");
 
+async function startServer(){
+    try{
+        await MongoDB.connect(config.db.uri);
+        console.log("Connect to the database!");
+
+        const PORT = config.app.port;
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    }   catch(error) {
+            console.log("Cannot connect to the database!", error);
+            process.exit();
+    }
+}
 //start server
-const PORT = config.app.port;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-});
+// const PORT = config.app.port;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}.`);
+// });
+startServer();
